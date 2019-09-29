d8468f17579974d4487d2bd5366f5efafce067ca
Autri Baghkhanian <autri.baghkhanian@gmail.com>
2019-09-29 08:41:59 -0700

    charts and messaging added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 const alert = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
 
 // create the html for the banner
 alert.innerHTML =
@@ -12,5 +18,113 @@ alert.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
         alert.style.display = "none"
+    }
+});
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+             "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+               2500],
+        backgroundColor: 'rgba(118, 215, 196, .3)',
+        borderWidth: 1,
+    }]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend : {
+        display: false
+    }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+// data for daily traffic bar chart
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#1ABC9C',
+        borderWidth: 1
+    }]
+};
+    
+const dailyOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend : {
+        display: false
+    }
+}
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#1ABC9C',
+            '#3498DB',
+            '#8E44AD'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+send.addEventListener('click', () => {
+    event.preventDefault();
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+        window.alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+        window.alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+        window.alert("Please fill out message field before sending");
+    } else {
+        window.alert(`Message successfully sent to: ${user.value}`);
     }
 });
